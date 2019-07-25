a8bbc23f512918d586e1c632814644ce069c6dee
emanuel <erodriguez@flowwest.com>
2019-07-25 10:34:01 -0700

    add in a sample of what a welcome modal can look like

--- server.R
+++ server.R
@@ -1,4 +1,11 @@
 function(input, output, session) {
+  # first visit modal
+  showModal(modalDialog(title = "Welcome!",
+                        taglist(
+                          tags$p("Something something something")
+                        ),
+                        easyClose = TRUE))
+  
   callModule(about_server, 'app')
   callModule(home_server, 'app')
 }
