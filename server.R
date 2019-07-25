2a6bdb77c81682e319ecffb04c864acf1aab945c
emanuel <erodriguez@flowwest.com>
2019-07-25 10:37:11 -0700

    small update of text on the sample welcome modal

--- server.R
+++ server.R
@@ -1,8 +1,10 @@
 function(input, output, session) {
   # first visit modal
   showModal(modalDialog(title = "Welcome!",
-                        taglist(
-                          tags$p("Something something something")
+                        tagList(
+                          tags$p("Something something something"), 
+                          tags$p("Is having a modal popup on the first"), 
+                          tags$p("launch helpful? {insert thinking emoji here}")
                         ),
                         easyClose = TRUE))
   
