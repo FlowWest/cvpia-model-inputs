a975ad85c1109d0106debd8c3f52de0d38719f59
emanuel <erodriguez@flowwest.com>
2019-09-04 13:37:13 -0700

    filter data to just years of simulation

--- global.R
+++ global.R
@@ -2,6 +2,8 @@ library(shiny)
 library(shinythemes)
 library(shinycssloaders)
 library(plotly)
+library(magrittr)
+library(lubridate)
 
 library(tidyverse)
 
@@ -17,12 +19,16 @@ metadata_lookup <- read_csv('data/metadata-lookup.csv')
 
 # category data frames
 # habitat <- read_rds('data/habitat.rds')
-flows <- read_rds('data/flows.rds')
-temperatures <- read_rds('data/temperatures.rds')
-habitat <- read_rds("data/habitat_with_scales.rds")
+flows <- read_rds('data/flows.rds') %>% 
+  filter(year(date) %in% 1980:1999)
+temperatures <- read_rds('data/temperatures.rds') %>% 
+  filter(year(date) %in% 1980:1999)
+habitat <- read_rds("data/habitat_with_scales.rds") %>% 
+  filter(year(date) %in% 1980:1999)
 
 pretty_num <- function(num, places = 2) {
   format(round(num, places), big.mark = ',', drop = FALSE)
 }
 
+pal <- c("#377eb8", "#1b7837")
 
