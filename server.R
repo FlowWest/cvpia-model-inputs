ae075b8be6fa6364be051083f54828a107726a88
emanuel <erodriguez@flowwest.com>
2019-09-04 13:37:58 -0700

    update the modal text

--- server.R
+++ server.R
@@ -2,7 +2,14 @@ function(input, output, session) {
   # first visit modal
   showModal(modalDialog(title = "Welcome!",
                         tagList(
-                          tags$p("This web-tool was designed to explore inputs to the Science Integration Team’s salmon population model. Use the tool to compare model inputs to your understanding of CVPIA watersheds, develop insights, and / or make suggestions to improve the model. Please help us make this more useful!") 
+                          tags$p("This web-tool was designed to explore inputs to the Science Integration Team’s salmon population model."),
+                          tags$p("Use the tool to:"),
+                          tags$ul(
+                            tags$li("compare model inputs to your understanding of CVPIA watersheds"),
+                            tags$li("develop insights"),
+                            tags$li("make suggestions to improve the model")
+                          ),
+                          tags$p("Please help us make this more useful!")
                         ),
                         easyClose = TRUE))
   callModule(about_server, 'app')
