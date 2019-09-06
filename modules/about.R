48650b0bda505451597a00d0e839d11b7b417a22
emanuel <erodriguez@flowwest.com>
2019-09-06 09:03:17 -0700

    add flowwest logo

--- modules/about.R
+++ modules/about.R
@@ -7,7 +7,17 @@ about_ui <- function(id) {
       column(width = 3),
       column(width = 6, 
              tags$h1('About'),
-             includeMarkdown("templates/about.md")
+             includeMarkdown("templates/about.md"), 
+             tags$div(id = "brand",
+                      tags$hr(),
+                      tags$a(tags$img(src = 'TransLogoTreb.png', width = 200, style = "display: block; margin: 0 auto;"),
+                             href = 'http://www.flowwest.com/', target = '_blank'),
+                      tags$h4('App created and maintained by',
+                              tags$a('Emanuel Rodriguez', href = 'mailto:erodriguez@flowwest.com', target = '_blank'),
+                              "and",
+                              tags$a('Sadie Gill', href = 'mailto:sgill@flowwest.com', target = '_blank'),
+                              style = "text-align: center;")
+             )
              ), 
       column(width = 3)
     )
