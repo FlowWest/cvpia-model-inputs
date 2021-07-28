ba6a109915834a59b8d0997c99355cfc681c62ec
ErinCain <ecain@flowwest.com>
2021-07-28 08:03:40 -0700

    Adds watershed labels to prep data script

--- data/prep-data.R
+++ data/prep-data.R
@@ -2,6 +2,17 @@ library(tidyverse)
 library(lubridate)
 library(stringr)
 
+# Watershed labels
+watershed_labels <- c("Upper Sacramento River", "Antelope Creek", "Battle Creek", 
+                      "Bear Creek", "Big Chico Creek", "Butte Creek", "Clear Creek", 
+                      "Cottonwood Creek", "Cow Creek", "Deer Creek", "Elder Creek", 
+                      "Mill Creek", "Paynes Creek", "Stony Creek", "Thomes Creek", 
+                      "Upper-mid Sacramento River", "Sutter Bypass", "Bear River", 
+                      "Feather River", "Yuba River", "Lower-mid Sacramento River", 
+                      "Yolo Bypass", "American River", "Lower Sacramento River", "Calaveras River", 
+                      "Cosumnes River", "Mokelumne River", "Merced River", "Stanislaus River", 
+                      "Tuolumne River", "San Joaquin River")
+
 # repeat for all of the habitat arrays
 
 # HABITAT ======================================================================
